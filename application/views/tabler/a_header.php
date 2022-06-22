ce38dc6947b5f0ebebaa8e1cdbb7315658fda44b
aslamalim <aslamalim@gmail.com>
2022-06-22 22:54:45 +0900

    Pengantian nama menu menjadi permintaan perbaikan

--- application/views/tabler/a_header.php
+++ application/views/tabler/a_header.php
@@ -157,7 +157,7 @@
                   </svg>
                 </span>
                 <span class="nav-link-title">
-                  Kerusakan
+                  Permintaan Perbaikan
                 </span>
               </a>
               <div class="dropdown-menu <?php echo $link == 'nrutin' ? 'show ' : ''; ?>">
