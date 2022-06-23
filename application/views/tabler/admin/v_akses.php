9e6807625506aa37f4f43ed7f7fd314aa6d46b8c
aslamalim <aslamalim@gmail.com>
2022-06-23 10:10:16 +0900

    Update Keternangan pd Hak akses

--- application/views/tabler/admin/v_akses.php
+++ application/views/tabler/admin/v_akses.php
@@ -39,7 +39,7 @@
                             <tr>
                                 <td class="text-center">1</td>
                                 <td>Teknisi</td>
-                                <td></td>
+                                <td>Teknisi/Engginering</td>
                                 <td><a href="#" class="btn btn-icon text-primary btn-light " c-aksi="update" c-spc=0><svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
                                             <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                                             <path d="M9 7h-3a2 2 0 0 0 -2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2 -2v-3" />
@@ -50,7 +50,7 @@
                             <tr>
                                 <td class="text-center">2</td>
                                 <td>Admin</td>
-                                <td></td>
+                                <td>Admin Aplikasi</td>
                                 <td><a href="#" class="btn btn-icon text-primary btn-light" c-aksi="update" c-spc=1><svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
                                             <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                                             <path d="M9 7h-3a2 2 0 0 0 -2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2 -2v-3" />
