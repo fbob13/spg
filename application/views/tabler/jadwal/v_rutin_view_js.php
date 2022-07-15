e4a647664f8ae68034383dbe0d4021aaf103e0ca
frits <frits130by@gmail.com>
2022-07-15 20:32:03 +0800

    upd keterangan approve

--- application/views/tabler/jadwal/v_rutin_view_js.php
+++ application/views/tabler/jadwal/v_rutin_view_js.php
@@ -255,6 +255,7 @@
             } else if (aksi == 'approve') {
                 $('#id-approve').val(data['id_rutin']);
                 $('#modal-konfirmasi-approve').modal('show');
+                clear_form("approve-keterangan-new")
 
                 $('#approve-nama').text(data['nama_teknisi']);
                 $('#approve-jadwal').text(data['tanggal_jadwal']);
